54421d82f2ee4c3d7e65501cf5f33cfbe53a67e8
Peter Atkinson <pdatkinson@rogers.com>
2015-01-09 01:47:44 +0100

    assignment1.html done + assignment2draft + changed button css in both files + JS for table row addition

---
+++ propertable.js
@@ -0,0 +1,37 @@
+$("form").submit(function(event) {
+  event.preventDefault()
+  var formObj = $(this)
+  var formURL = formObj.attr("action")
+  var formData = new FormData(this)
+
+  $.ajax({
+    url: formURL,
+    type: 'POST',
+    data: formData,
+    mimeType: "multipart/form-data",
+    contentType: false,
+    cache: false,
+    processData: false,
+    success: function(data, textStatus, jqXHR) {
+      console.log(data) //returned api call.
+      data = JSON.parse(data)
+      $.getJSON(data.URI, function(data) {
+        addItemToList(data)
+      })
+    },
+    error: function(jqXHR, textStatus, errorThrown) {}
+  })
+})
+
+function addItemToList(item) { // from here http://stackoverflow.com/questions/18333427/how-to-insert-row-in-html-table-body-in-javascript
+  var table = document.getElementById("tabled")
+  var row = table.insertRow(table.rows.length - 2)
+
+  var cell1 = row.insertCell(0).innerHTML = item.category
+  var cell2 = row.insertCell(1).innerHTML = item.name
+  var cell3 = row.insertCell(2).innerHTML = item.amount
+  var cell4 = row.insertCell(3).innerHTML = item.date
+  var cell5 = row.insertCell(4).innerHTML = item.location
+
+  document.forms[0].reset()
+}
