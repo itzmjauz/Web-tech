f43d7e128cbc4c1403af1da58fd9a7d937df686b
Peter Atkinson <pdatkinson@rogers.com>
2015-01-12 19:00:19 +0100

    table sorting + date widget for assignment2
     propertable.js renamed to tablemanipulation.js.

--- tablemanipulation.js
+++ tablemanipulation.js
@@ -24,13 +24,14 @@ $("form").submit(function(event) {
 })
 
 function addItemToList(item) { // from here http://stackoverflow.com/questions/18333427/how-to-insert-row-in-html-table-body-in-javascript
-  var table = document.getElementById("tabled")
-  ,   row = table.insertRow(table.rows.length - 2)
-  ,   cell1 = row.insertCell(0).innerHTML = item.category
-  ,   cell2 = row.insertCell(1).innerHTML = item.name
+  var table = document.getElementById("tabledbody")
+  ,   row = table.insertRow(table.rows.length)
+  ,   cell1 = row.insertCell(0).innerHTML = item.name
+  ,   cell2 = row.insertCell(1).innerHTML = item.category
   ,   cell3 = row.insertCell(2).innerHTML = item.amount
   ,   cell4 = row.insertCell(3).innerHTML = item.date
   ,   cell5 = row.insertCell(4).innerHTML = item.location
 
   document.forms[0].reset()
+  $("#tabled").tablesorter();
 }
