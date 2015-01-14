6d68475e73cf95942c1074f8399323b38a5c13ee
itzmjauz <itzmjauz@gmail.com>
2015-01-14 18:25:31 +0100

    gotta find a way around forEach

--- sort.js
+++ sort.js
@@ -3,23 +3,34 @@ var elems = document.querySelectorAll('#tabled #tabledbody tr')
   , sortb = document.querySelectorAll('#tabled thead th')
 
 
-// className assignment for readability 
-sortb[0].className = "sort1"
-sortb[1].className = "sort2"
-sortb[2].className = "sort3"
-sortb[3].className = "sort4"
-sortb[4].className = "sort5"
+  // className assignment for readability 
+sortb[0].className = "sort0"
+sortb[1].className = "sort1"
+sortb[2].className = "sort2"
+sortb[3].className = "sort3"
+sortb[4].className = "sort4"
 
 // assign onclick events
+[0, 1, 2, 3, 4].forEach(function(ind) {
 
-sortb[0].onclick = function(){
-  if(this.className == "sort1"){
-    this.className = "sortUp"
-    reset(0)
-    // sort etc, flip table indexes. 
+  sortb[ind].onclick = function() {
+    if (this.className == "sort" + ind) {
+      this.className = "sortUp"
+      reset(ind)
+      // sort etc, flip table indexes. 
+    }
   }
-}
+})
 
-function reset(index){
-  
+function reset(index) {
+  I = [0, 1, 2, 3, 4]
+
+  I.forEach(function(ind) {
+    if (ind == index) {
+      //do nothing
+    }
+    else {
+      sortb[ind].className = "sort" + ind
+    }
+  })
 }
