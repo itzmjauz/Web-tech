52c1b1354e797a0bf0d02132cc1b23b6b2d24519
itzmjauz <itzmjauz@gmail.com>
2015-01-14 14:53:10 +0100

    test update

--- sort.js
+++ sort.js
@@ -1,8 +1,9 @@
-var items = ['abc', 'bcd', 'zxc']
-  , elems = [1, 2, 3, 4]
-
-
 // selects all rows, dont work with the first + last two rows. so
 var elems = document.querySelectorAll('#tabled #tabledbody tr')
+  , sortb = document.querySelectorAll('#tabled thead th')
 
-
+sortb.forEach(function(item) {
+  item.onclick = function() {
+    console.log(item.innerHTML)
+  }
+})
