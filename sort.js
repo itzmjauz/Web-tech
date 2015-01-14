1755c848e26c0db944ff3a4ec97c7dd740e99df4
itzmjauz <itzmjauz@gmail.com>
2015-01-14 14:45:51 +0100

    start on sort algorithm

---
+++ sort.js
@@ -0,0 +1,8 @@
+var items = ['abc', 'bcd', 'zxc']
+  , elems = [1, 2, 3, 4]
+
+
+// selects all rows, dont work with the first + last two rows. so
+var elems = document.querySelectorAll('#tabled #tabledbody tr')
+
+
