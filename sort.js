69c0f69e129041e106032939cd090ba0ac7e9026
itzmjauz <itzmjauz@gmail.com>
2015-01-14 17:33:00 +0100

    dynamic classes for sorting

--- sort.js
+++ sort.js
@@ -2,8 +2,24 @@
 var elems = document.querySelectorAll('#tabled #tabledbody tr')
   , sortb = document.querySelectorAll('#tabled thead th')
 
-sortb.forEach(function(item) {
-  item.onclick = function() {
-    console.log(item.innerHTML)
+
+// className assignment for readability 
+sortb[0].className = "sort1"
+sortb[1].className = "sort2"
+sortb[2].className = "sort3"
+sortb[3].className = "sort4"
+sortb[4].className = "sort5"
+
+// assign onclick events
+
+sortb[0].onclick = function(){
+  if(this.className == "sort1"){
+    this.className = "sortUp"
+    reset(0)
+    // sort etc, flip table indexes. 
   }
-})
+}
+
+function reset(index){
+  
+}
