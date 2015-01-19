7163a06e20aa1bab05433bf6a51740a8ade2da0d
itzmjauz <itzmjauz@gmail.com>
2015-01-19 13:22:34 +0100

    extra functionality, remove old stuff

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 import os.path
 import sqlite3
+import json
 
 #change to liking
 filename = "Inventory.db"
 
+#create database if it doens't exist
 if(not os.path.isfile(filename)):
     db = sqlite3.connect(filename)
     db.execute("""
@@ -19,6 +21,22 @@ if(not os.path.isfile(filename)):
     """)
 
     db.commit()
+else:
+    db = sqlite3.connect(filename)
+
+#db.execute("INSERT INTO inventory VALUES (1,'Banana','Fruit','Amsterdam','2014-10-05',15)")
+#db.execute("INSERT INTO inventory VALUES (2,'Apple','Fruit','Amsterdam','2014-02-05',58)")
+#db.commit()
+
+db.row_factory = sqlite3.Row
+c = db.cursor()
 
 
-        
+
+def fetchAll():
+    rows = db.execute("select * from inventory").fetchall()
+    db.commit()
+    print(json.dumps( [dict(ix) for ix in rows] ))
+
+
+
