a8277f4fb7d468f3c2e2308291d2ea839919e054
itzmjauz <itzmjauz@gmail.com>
2015-01-21 00:49:21 +0100

    remove print

--- bottletutorial/todo.py
+++ bottletutorial/todo.py
@@ -13,7 +13,6 @@ from bottle import default_app
 @hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
-    print response.headers['Access-Control-Allow-Origin']
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
